bf6bf36061ed99055a9c325982f92df864e3448f
samah.moumen <samah_moumen@um5.ac.ma>
2026-01-29 23:14:28 +0100

    CD deployment

--- deploy.sh
+++ deploy.sh
@@ -8,17 +8,27 @@ ssh -p "$VM_PORT" -o StrictHostKeyChecking=no "$VM_USER@$VM_HOST" \
 '#!/bin/bash
 set -e
 
+if [[ -z "$PROJECT_DIR" || "$PROJECT_DIR" == "/" || "$PROJECT_DIR" == "$HOME" ]]; then
+    echo "Error: PROJECT_DIR ($PROJECT_DIR) is unsafe or undefined!"
+    exit 1
+fi
 echo "-> Création du dossier projet"
 mkdir -p "$PROJECT_DIR"
 cd "$PROJECT_DIR"
+[[ -z "$PROJECT_DIR" || "$PROJECT_DIR" == "/" ]] && echo "PROJECT_DIR is unsafe!" && exit 1
 
 echo "-> Clonage du repo (main)"
 if [ -d .git ]; then
-    git reset --hard
-    git pull origin main
+    echo "Repo existant : Reset et Pull"
+    git fetch origin main
+    git reset --hard origin/main
+elif [ -z "$(ls -A .)" ]; then
+    echo "Dossier vide : Clonage du repo"
+    git clone -b main "$REPO_URL" .
 else
-    git clone -b main $REPO_URL .
+    echo "Dossier non vide et pas un dépôt Git. Aucune action effectuée."
 fi
+
 
 echo "-> Création du fichier .env"
 echo "$ENV_FILE" > .env
